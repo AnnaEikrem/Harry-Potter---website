cfe3c94ff24fc1df13e960aa16990beac91532aa
Anna Eikrem <eikrem.anna@gmail.com>
2022-11-26 10:12:35 +0100

    Added burger menu function and changes to header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import Header from './modules/header.js';
+import Hero from './modules/hero.js';
 	
 Header();
+Hero();
--- js/modules/header.js
+++ js/modules/header.js
@@ -1,3 +1,26 @@
 export default function Header() {
+	let navigationVisible = false;
 
+	const headerMenuButton = document.querySelector('.header__menu-button');
+	const headerNavigation = document.querySelector('.header__navigation');
+
+	headerMenuButton.addEventListener('click', handleHeaderMenuButtonClick);
+
+	/* Toggles visibility class on 'click' event */
+	function handleHeaderMenuButtonClick(event) {
+		toggleNavigationVisibility();
+		renderHTML();
+	}
+
+	function toggleNavigationVisibility() {
+		navigationVisible = !navigationVisible;
+	}
+
+	function renderHTML() {
+		if (navigationVisible === true) {
+			headerNavigation.classList.add('header__navigation--visible');
+		} else {
+			headerNavigation.classList.remove('header__navigation--visible');
+		}
+	}
 }
