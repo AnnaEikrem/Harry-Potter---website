78939cc429e11687a0664128e56999214f6badb5
Anna Eikrem <eikrem.anna@gmail.com>
2022-11-27 16:06:04 +0100

    Added cursor (js and css)

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ import Header from './modules/header.js';
 import Hero from './modules/hero.js';
 import Sortinghat from './modules/sortingHat.js';
 import characterSlideshow from './modules/characterSlideshow.js';
+
+import Cursor from './modules/cursor.js';
 	
 Header();
 // Hero();
 Sortinghat();
 characterSlideshow();
+
+
+
+// Cursor();
