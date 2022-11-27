0f89ffcbf265148ef4e78a1a1809ff69488de179
Anna Eikrem <eikrem.anna@gmail.com>
2022-11-27 14:01:37 +0100

    Added CharacterSlideshow structure

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import Header from './modules/header.js';
 import Hero from './modules/hero.js';
 import Sortinghat from './modules/sortingHat.js';
+import characterSlideshow from './modules/characterSlideshow.js';
 	
 Header();
 // Hero();
 Sortinghat();
+characterSlideshow();
