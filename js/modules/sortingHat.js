1acb22cb712c0750f3f523f44f7b9e2165aea21b
Anna Eikrem <eikrem.anna@gmail.com>
2022-11-27 13:55:35 +0100

    Added Sortinghat (index, js and css)

--- js/modules/sortingHat.js
+++ js/modules/sortingHat.js
@@ -1,3 +1,34 @@
 export default function Sortinghat() {
-	
+	const sortinghat = document.querySelector('.sortinghat__random-picker');
+	const outputField = document.querySelector('.sortinghat__result');
+	const clickButton = document.querySelector('.sortinghat__click-button');
+	const houseArray = [
+		'Gryffindor',
+		'Hufflepuff',
+		'Ravenclaw',
+		'Slytherin'
+	];
+	let outputMessage;
+
+	if (sortinghat) {
+		clickButton.addEventListener('click', handleClickButton);
+	}
+
+	function handleClickButton(event) {
+		getHouseResult();
+		renderHTML();
+	}
+ 
+	function getHouseResult() {
+		const randomIndex = Math.floor(Math.random() * houseArray.length);
+		outputMessage = houseArray[randomIndex];
+		console.log(outputMessage);
+
+		return outputMessage;
+	}
+
+	function renderHTML() {
+		outputField.innerHTML = ` ${outputMessage}`;
+	}
+
 }
